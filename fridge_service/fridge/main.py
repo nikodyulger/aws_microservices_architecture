ef0565edd9594b85b3063d2fd7bfeb31f14a70b5
nikodyulger <nikolasvdulgerov@gmail.com>
2023-03-13 21:30:19 +0100

    FridgeRouter, Schemas and Debugging mode #4

--- fridge_service/fridge/main.py
+++ fridge_service/fridge/main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
-fridge = FastAPI()
+from fridge.schemas import Product
 
-@fridge.get('/')
+fridge = FastAPI(title="Fridge API")
+
+fridge_router = APIRouter()
+
+@fridge_router.get('/')
 def open_fridge():
     return "Hi! You opened the fridge!"
+
+fridge.include(fridge_router)
+
+if __name__ == "__main__":
+    # DEBUGGING
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
